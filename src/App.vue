cc81531940f75c4565ff6d0dcca36fb2cc86c6c0
Ali Kooshesh <alikooshesh007@gmail.com>
2023-05-31 11:17:19 +0330

    feat: add game data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,53 @@
 <script>
+import {words} from './static_data/words'
+import GameDataForm from './components/GameDataForm.vue';
+export default{
+  components : {
+    GameDataForm
+  },
+  data(){
+    return {
+      words,
+      gameStep : 0,
+      gameData : {
+        playerCount : null,
+        spyCount : null,
+        selectedWord : null,
+        messageList : []
+      }
+    }
+  },
+  watch : {
+    gameStep(){
+      console.log(this.gameData)
+    }
+  },
+  methods:{
+    setGameData(playerCount , spyCount){
+      if(playerCount && spyCount){
+        const tempWord = this.words[Math.floor(Math.random()*this.words.length)];
 
+        this.playerCount = playerCount
+        this.spyCount = spyCount
+        this.selectedWord = tempWord
+
+        const tempMessageList = []
+
+        for(let i = 0;i < playerCount ; i++){
+          tempMessageList.push(tempWord.word)
+        }
+
+        for(let i = 0;i < spyCount ; i++){
+          tempMessageList[Math.floor(Math.random()*tempMessageList.length)] = "****"
+        }
+        this.gameData.messageList = tempMessageList
+        this.gameStep = 1
+      }
+    }
+  }
+}
 </script>
 
 <template>
-  
+  <GameDataForm :setGameData="setGameData"/>
 </template>
---
+++ src/components/GameDataForm.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    props:{
+        setGameData : {
+            type : Function,
+            required : true
+        }
+    },
+    data(){
+        return{
+            playerCount: "",
+            spyCount : ""
+        }
+    },
+    methods : {
+        handleClick(){
+            this.setGameData(this.playerCount,this.spyCount)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="w-full max-w-[85vw] mx-auto">
+        <div class="flex flex-col gap-[16px]">
+            <div class="flex flex-col gap-[8px]">
+                <label>تعداد بازیکن</label>
+                <input type="number" v-model="playerCount"/>
+            </div>
+
+            <div class="flex flex-col gap-[8px]">
+                <label>تعداد جاسوس</label>
+                <input type="number" v-model="spyCount"/>
+                <small>تعداد پیشنهادی جاسوس ۳۰٪ کل بازیکنان است.</small>
+            </div>
+
+            <button @click="handleClick">شروع بازی</button>
+        </div>
+    </div>
+</template>
